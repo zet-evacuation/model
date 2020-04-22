5025dba95d8e2a56b4d0c0cbec84230ff14c6624
Jan-Philipp Kappmeier <jp.kappmeier@gmail.com>
2020-04-23 00:59:50 +0200

    Converted vrom NetBeans/ant to Maven project

---
+++ src/test/groovy/de/zet_evakuierung/model/BuildingPlanTest.groovy
@@ -0,0 +1,39 @@
+/* zet evacuation tool copyright (c) 2007-15 zet evacuation team
+ *
+ * This program is free software; you can redistribute it and/or
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package de.zet_evakuierung.model
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test
+import spock.lang.Specification
+
+/**
+ *
+ * @author Jan-Philipp Kappmeier
+ */
+class TestBuildingPlanTest extends Specification {
+    def bp = new BuildingPlan()
+
+    def "contains default floor"() {
+        expect:
+        bp.getDefaultFloor() != null
+        bp.getFloors().size() == 1
+    }
+  
+}
